81a875c9e1f98651ec53dfda28f3da0313dec725
amir <amirisrael22@gmail.com>
2022-05-16 08:20:53 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import useDebounce from "./hooks/useDebounce";
+import Axios from 'axios'
 
 function App() {
+
+  const inputRef = useRef()
+
+  const [state, setState] = useState({
+    current_value: "",
+    data: null
+  })
+
+  const debounceValue = useDebounce(state.current_value)
+
+  const handleChanges = e => {
+    setState({ ...state, current_value: e.target.value })
+  }
+
+  useEffect(() => {
+    inputRef.current.focus()
+  }, [])
+
+  useEffect(() => {
+    if (debounceValue) handleFetchData(debounceValue)
+    if (!debounceValue) setState({ ...state, data: null })
+  }, [debounceValue])
+
+  const handleFetchData = value => {
+    Axios.get(`https://api.nationalize.io/?name=${value}`)
+      .then(res => {
+        console.log(res.data)
+        setState({ ...state, data: res.data })
+      })
+      .catch(err => {
+        alert(err.message)
+      })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <input ref={inputRef} value={state.current_value} onChange={handleChanges} />
+
+      <table>
+
+        <thead>
+          <tr>
+            <th>Current Value</th>
+            <th>{state.current_value}</th>
+          </tr>
+          <tr>
+            <th>Debounce Value</th>
+            <th>{debounceValue}</th>
+          </tr>
+        </thead>
+
+        <thead>
+          <tr>
+            <th className="regular">country</th>
+            <th className="regular">prob</th>
+          </tr>
+        </thead>
+        <tbody>
+          {state.data && state.data.country?.map((country, index) => <tr key={index}>
+            <td>{country.country_id}</td>
+            <td>{Math.round(country.probability * 100)}%</td>
+          </tr>)}
+        </tbody>
+      </table>
+
+      {state.data && state.data.country.length === 0 && <p id="no-countries">No countries found</p>}
+
     </div>
   );
 }
---
+++ src/hooks/useDebounce.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react'
+
+export default function useDebounce(value, time = 1000) {
+    const [debounceValue, setDebounceValue] = useState(value)
+
+    useEffect(() => {
+        let timer;
+
+        timer = setTimeout(() => {
+            setDebounceValue(value)
+        }, time)
+
+        return () => {
+            console.log('clear timer')
+            clearTimeout(timer)
+        }
+    }, [value])
+
+    return debounceValue
+}
